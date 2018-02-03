8186796ec702ed500993a8552ca59c4e63673961
ShellingfordX <34960057+ShellingfordX@users.noreply.github.com>
2018-02-04 03:04:07 +0900

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -16,3 +16,6 @@ sudo apt install -y git shutter gnome-web-photo libgoo-canvas-perl libimage-exif
 
 # Peek のインストール
 sudo add-apt-repository ppa:peek-developers/stable -y && sudo apt update && sudo apt install -y peek
+
+# プロファイルのアイコン取得
+cd && wget -O .face https://farm5.staticflickr.com/4620/40030405722_60edd303e1_o.png
