c9442a75babd329db9da06a3591de6a3bd1979b1
ShellingfordX <34960057+ShellingfordX@users.noreply.github.com>
2019-02-07 00:52:48 +0900

    Update preview.sh

--- Plymouth/preview.sh
+++ Plymouth/preview.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 if [ $EUID -ne 0 ]; then
-	echo ERROR: You must run this as root
+	echo エラー: sudo 付きで実行して下さい。
 	exit
+fi
+
+# 依存関係を満たす
+if [ $(dpkg-query -W -f='${Status}' plymouth-x11 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	sudo apt install -y plymouth-x11
 fi
 
 sudo plymouthd
